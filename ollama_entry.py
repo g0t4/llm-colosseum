a249995e187804ccceb435bbe0a6407a9220defc
Wes Higbee <wes.mcclure@gmail.com>
2024-07-26 16:19:48 -0500

    suppres superfluous httpx logs from ollama API calls, I am observing ollama serve to see that

--- ollama_entry.py
+++ ollama_entry.py
@@ -3,9 +3,11 @@ import sys
 from dotenv import load_dotenv
 from eval.game import Game, Player1, Player2
 from loguru import logger
+import logging
 
 logger.remove()
 logger.add(sys.stdout, level="INFO")
+logging.getLogger('httpx').setLevel(logging.WARNING)  # suppress httpx logs from ollama package HTTP API calls
 
 load_dotenv()
 
